38099a56732a89391219f53e50ae1486ea63add3
BinTran04 <tranquocnhut2004@gmail.com>
2026-03-20 15:50:03 +0700

    Initial commit with Dockerfile

---
+++ src/main/java/com/example/TranQuocNhut/TranQuocNhutApplication.java
@@ -0,0 +1,13 @@
+package com.example.TranQuocNhut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranQuocNhutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TranQuocNhutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TranQuocNhut/TranQuocNhutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TranQuocNhut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranQuocNhutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
